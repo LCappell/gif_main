8e5e8d440cc89adad2ecd91a7f34dc8fcc804f2b
Louis Cappell <l.cappell@icloud.com>
2021-05-25 11:52:59 +0100

    Added api key variable and new env file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import axios from "axios";
 import SearchForm from "./Components/SearchForm";
 import GifList from "./Components/GifList";
 
+const apiKey = process.env.REACT_APP_API_KEY;
+
 export default class App extends Component {
   constructor() {
     super();
@@ -20,7 +22,7 @@ export default class App extends Component {
   performSearch = (query) => {
     axios
       .get(
-        `http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`
+        `http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=${apiKey}`
       )
       .then((response) => {
         this.setState({
